c5f047ab59fb3d7afe6a9dd72e49dc2791b7dad5
MiddleFork <walker@mfgis.com>
2015-10-14 08:52:08 -0700

    one_to_one fix
    An adjunct to chrisdev:master #53 ORs together tests for one_to_many and one_to_one

--- django_pandas/io.py
+++ django_pandas/io.py
@@ -17,7 +17,7 @@ def to_fields(qs, fieldnames):
                         model = field.model
                         break
             else:
-                if hasattr(field, "one_to_many") and field.one_to_many:
+                if (hasattr(field, "one_to_many") and field.one_to_many) or (hasattr(field, "one_to_one") and field.one_to_one):
                     model = field.related_model
                 elif field.get_internal_type() in ('ForeignKey', 'OneToOneField', 'ManyToManyField'):
                     model = field.rel.to
